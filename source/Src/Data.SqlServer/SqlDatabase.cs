f8c3c481020ed61bd2d0cf7600666a0c553c6580
Nelson Vásquez <nvasquez@aseinfo.com.sv>
2024-02-06 16:32:27 -0600

    Iniciando migración de libreía System.Data.SqlClient a Microsof.Data.SqlClient

--- source/Src/Data.SqlServer/SqlDatabase.cs
+++ source/Src/Data.SqlServer/SqlDatabase.cs
@@ -3,7 +3,7 @@
 using System;
 using System.Data;
 using System.Data.Common;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using System.Xml;
 using Microsoft.Practices.EnterpriseLibrary.Common.Configuration;
 using Microsoft.Practices.EnterpriseLibrary.Common.Utility;
